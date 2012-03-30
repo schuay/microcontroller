9f67af230652ce8b6c8a80b90865ab738a62ed96
Jakob Gruber <jakob.gruber@gmail.com>
2012-03-30 10:07:39 +0200

    Basic makefiles for libs
    util.h was missing from original source archives.

--- phase2/pong/lib/libwiimote/uart/hal_wt41_fc_uart.h
+++ phase2/pong/lib/libwiimote/uart/hal_wt41_fc_uart.h
@@ -2,7 +2,7 @@
 #define HAL_WT41_FC_UART
 
 #include <stdint.h>
-#include <util.h>
+#include "util.h"
 
 error_t halWT41FcUartInit(void (*sndCallback)(), void (*rcvCallback)(uint8_t));
 error_t halWT41FcUartSend(uint8_t byte);
