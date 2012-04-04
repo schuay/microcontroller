5d664fcebdf9b16cd8e3b857b008a99cdd237bae
Jakob Gruber <jakob.gruber@gmail.com>
2012-04-04 18:40:05 +0200

    Test ADC in main()

--- phase2/pong/src/main.c
+++ phase2/pong/src/main.c
@@ -5,17 +5,27 @@
 #include "uart_streams.h"
 #include "timer.h"
 #include "lcd.h"
+#include "adc.h"
 
 static volatile uint8_t cntr = 0;
 
 void printmsg(void) {
+    adc_start_conversion();
     printf_P(PSTR("Hello, world! %d\n"), ++cntr);
+}
+
+void adc_done(uint16_t result) {
+    printf_P(PSTR("ADC result: %d\n"), result);
 }
 
 int main(void) {
     sleep_enable();
     uart_streams_init();
     lcd_init();
+
+    struct adc_conf ac = { adc_done };
+    adc_init(&ac);
+
     sei();
 
     struct timer_conf conf = { 1, 1000, printmsg };
