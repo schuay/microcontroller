b19b197e0cde3708d610bab5d14a307bc0faabf4
Jakob Gruber <jakob.gruber@gmail.com>
2012-04-09 10:01:43 +0200

    Bluetooth WIP

--- phase2/pong/src/bt_hal.c
+++ phase2/pong/src/bt_hal.c
@@ -22,14 +22,10 @@ enum BTPins {
 #define RB_SIZE (128)
 static ringbuffer_t *_rbuf;
 
-static void _pullPJ5High(void) {
-    /* TODO: only enable DataRegEmptyIntr here (and remove it from below).
-     */
-    set_bit(PORTJ, PJ5);
-}
-
 /* TODO: debug. */
 static recv_handler_t _recv_callback;
+static intr_handler_t _send_callback;
+
 static void _recv_handler(uint8_t data) {
     /* OVERRUN flag uart */
     printf("bt recvd: %x\n", data);
@@ -41,7 +37,6 @@ static void _recv_handler(uint8_t data) {
     _recv_callback(data);
 }
 
-static intr_handler_t _send_callback;
 static void _send_handler(void) {
     if (PORTJ & _BV(RTS)) {
         /* Disable DataRegEmptyIntr;
@@ -49,6 +44,27 @@ static void _send_handler(void) {
          * When triggered, reenable DataRegEmptyIntr; */
     }
     _send_callback();
+}
+
+static void _bt_continue_init(void) {
+    /* Pull RST HIGH. */
+    set_bit(PORTJ, RST);
+
+    /* Init UART3. */
+#define BAUD 1000000
+#include <util/setbaud.h>
+    struct uart_conf uc = {
+        Uart3,
+        ReceiverEnable | TransmitterEnable | RXCompleteIntrEnable, // | DataRegEmptyIntrEnable,
+        UBRR_VALUE,
+#if USE_2X
+        true,
+#else
+        false,
+#endif
+        _send_handler,
+        _recv_handler };
+    uart_init(&uc);
 }
 
 error_t halWT41FcUartInit(intr_handler_t sndCallback,
@@ -59,8 +75,8 @@ error_t halWT41FcUartInit(intr_handler_t sndCallback,
     PORTJ &= ~(_BV(CTS) | _BV(RST)); 
     DDRJ = (DDRJ | _BV(CTS) | _BV(RST)) & ~_BV(RTS);
 
-    /* Pull P5 low for 5ms to reset bluetooth module. */
-    struct timer_conf tc = { Timer3, true, 5, _pullPJ5High };
+    /* Pull RST low for 5ms to reset bluetooth module. */
+    struct timer_conf tc = { Timer3, true, 5, _bt_continue_init };
     timer_set(&tc);
 
     _rbuf = ringbuffer_init(RB_SIZE);
@@ -69,21 +85,8 @@ error_t halWT41FcUartInit(intr_handler_t sndCallback,
     _recv_callback = rcvCallback;
     _send_callback = sndCallback;
 
-    /* Init UART3. */
-#define BAUD 1000000
-#include <util/setbaud.h>
-    struct uart_conf uc = {
-        Uart3,
-        ReceiverEnable | TransmitterEnable | RXCompleteIntrEnable | DataRegEmptyIntrEnable,
-        UBRR_VALUE,
-#if USE_2X
-        true,
-#else
-        false,
-#endif
-        _send_handler,
-        _recv_handler };
-    uart_init(&uc);
+    /* The rest of the initialization process takes place once the
+     * timer expires. */
 
     return SUCCESS;
 }
