f681766e2fce067e2c03718448045f4eedc74ecf
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-28 12:51:21 +0200

    Define ds1307_time_mem_t and rtc_time_t

--- phase3/rtc/HplDS1307.h
+++ phase3/rtc/HplDS1307.h
@@ -2,7 +2,25 @@
 #define HPLDS1307_H
 
 typedef struct {
-    uint8_t dummy;
+    uint8_t seconds :7;
+    uint8_t clockHalt :1;   /**< 0: Oscillator enabled, 1: disabled. */
+    uint8_t minutes :7;
+    uint8_t :1;
+    uint8_t hours :6;
+    uint8_t hour_mode :1;   /**< 0: 24h, 1: 12h. */
+    uint8_t :1;
+    uint8_t day :3;
+    uint8_t :5;
+    uint8_t date :6;
+    uint8_t :2;
+    uint8_t month :5;
+    uint8_t :3;
+    uint8_t year :8;
+    uint8_t rs :2;          /**< Rate Select: set to 11 by default. */
+    uint8_t :2;
+    uint8_t sqwe :1;        /**< Square-Wave Enable: set to 0 by default. */
+    uint8_t :2;
+    uint8_t out :1;         /**< Output Control: set to 0 by default. */
 } ds1307_time_mem_t;
 
 #endif
--- phase3/rtc/Rtc.h
+++ phase3/rtc/Rtc.h
@@ -2,7 +2,13 @@
 #define RTC_H
 
 typedef struct {
-    uint8_t dummy;
+    uint8_t seconds; /**< 0-59 */
+    uint8_t minutes; /**< 0-59 */
+    uint8_t hours;   /**< 0-23 */
+    uint8_t day;     /**< 1-7 */
+    uint8_t date;    /**< 1-31 */
+    uint8_t month;   /**< 1-12 */
+    uint8_t year;    /**< 0-99 */
 } rtc_time_t;
 
 #endif
